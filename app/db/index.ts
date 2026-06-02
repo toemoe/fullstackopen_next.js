17c3519abd569d15ef1a56465bb122f48b610b93
strangka <strangka@student.21-school.ru>
2026-06-02 15:18:33 +0700

    all

--- app/db/index.ts
+++ app/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/neon-http"
 import { neon } from "@neondatabase/serverless"
 import * as schema from "../../db/schema"
 
-const sql = neon(process.env.DATABASE_URL!)
+const sql = neon(process.env.BLOGS_STORAGE_DATABASE_URL!)
 export const db = drizzle(sql, {
   schema,
 })
